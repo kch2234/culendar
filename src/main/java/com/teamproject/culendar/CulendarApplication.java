8c042074cf8003bd028b4ee4b0de7a4032557a27
kch <https://github.com/kch2234/culendar.git>
2024-04-02 09:42:08 +0900

    Initial commit

---
+++ src/main/java/com/teamproject/culendar/CulendarApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.culendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamproject/culendar/CulendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.culendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
