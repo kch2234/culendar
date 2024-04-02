f63331135cae6591e8515fe2173aa268236eaaec
kch <https://github.com/kch2234/culendar.git>
2024-04-02 18:22:07 +0900

    Set sec

--- src/main/java/com/teamproject/culendar/config/SecurityConfig.java
+++ src/main/java/com/teamproject/culendar/config/SecurityConfig.java
@@ -1,37 +1,55 @@
 package com.teamproject.culendar.config;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collection;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-class SecurityConfigTest {
+public class SecurityConfig {
+
+    @Secured("ROLE_ADMIN")
+    public void testSecured() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            System.out.println(authority.getAuthority());
+        }
+    }
 
     // 시큐리티 필터 처리 설정 메서드 빈
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+        http.authorizeHttpRequests(request -> request.anyRequest().permitAll()
+                        .requestMatchers("/").hasRole("ROLE_ADMIN"))
                 .csrf(csrf -> csrf.disable())
-                .formLogin(login ->
+                .formLogin(login -> // 로그인 설정
                         login.loginPage("/login")
-                        .defaultSuccessUrl("/", true))
-                .logout(logout ->
+                                .defaultSuccessUrl("/", true)) // 로그인 성공 후 이동할 페이지
+                .logout(logout -> // 로그아웃 설정
                         logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                                 .logoutSuccessUrl("/")
-                                .invalidateHttpSession(true));
+                                .invalidateHttpSession(true)) // 로그아웃 시 세션 무효화
+                .exceptionHandling(exceptionHandling -> exceptionHandling // 접근 거부 페이지 설정
+                        .accessDeniedPage("/access-denied"));
         return http.build();
     }
 
