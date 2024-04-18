dc4445fed048934e06e148f2e4fe78a573762c6b
kch <https://github.com/kch2234/culendar.git>
2024-04-18 19:00:31 +0900

    fixed map in programDetail, boardDetail and working on searchHome

---
+++ src/main/java/com/teamproject/culendar/controller/SearchController.java
@@ -0,0 +1,16 @@
+package com.teamproject.culendar.controller;
+
+import com.teamproject.culendar.service.ProgramService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+@Slf4j
+@RequiredArgsConstructor
+public class SearchController {
+
+}
