accfebf8d0b389a6a5f5124fd559ef39237d9788
dye0ny6 <dye0ny6@gmail.com>
2024-04-23 11:47:17 +0900

    fix cancelEventApply

--- src/main/java/com/teamproject/culendar/repository/EventRepository.java
+++ src/main/java/com/teamproject/culendar/repository/EventRepository.java
@@ -3,11 +3,12 @@ package com.teamproject.culendar.repository;
 import com.teamproject.culendar.domain.board.EventMemberList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<EventMemberList, Long> {
 
   @Query("SELECT eml FROM EventMemberList eml WHERE eml.member.id = :memberId AND eml.eventBoard.id = :eventBoardId")
-  EventMemberList findEventMemberListByMemberIdAndEventBoardId(Long memberId, Long eventBoardId);
+  EventMemberList findEventMemberListByMemberIdAndEventBoardId(@Param("memberId") Long memberId, @Param("eventBoardId") Long eventBoardId);
 
 
 }
